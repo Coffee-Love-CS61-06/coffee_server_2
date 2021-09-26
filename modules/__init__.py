0f4c65682a31aaddc64fdb8d709d671b2c23d5a7
SAKDIPAT ONTOUM <61130500229@st.sit.kmutt.ac.th>
2021-09-26 18:16:44 +0700

    [ADD] mobilenet model

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,22 +1,22 @@
 from flask import Flask
 
-from modules import backed
+from modules import backed, api
 
 app = Flask(__name__)
-#UPLOAD_FOLDER = '/static/assets/uploads/covers/'
+UPLOAD_FOLDER = '/static/assets/uploads/covers/'
 
 
 
-#app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-# from tensorflow.keras.models import load_model
-#  load_model("imagemodel.h5")
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+from tensorflow.keras.models import load_model
+load_model("modules/model/mobilenet_model.hdf5")
 print("model loading .... plaese wait this might take a while")
 
 app.secret_key ='1234'
 from modules.backed.routes import mod
-from modules.api.routes import mod, api
+from modules.api.routes import mod
 
 # # register blueprints for api and site
 
 app.register_blueprint(backed.routes.mod)
-app.register_blueprint(api.routes.mod,url_prefix='/api')
+app.register_blueprint(api.routes.mod, url_prefix='/api')
--- run.py
+++ run.py
@@ -1,5 +1,6 @@
+from keras.models import load_model
 from modules import app
-# 
-# model = load_model("imagemodel.h5")
-# print("model loading .... plaese wait this might take a while")
+
+model = load_model("modules/model/mobilenet_model.hdf5")
+print("model loading .... plaese wait this might take a while")
 app.run(debug=False,host='192.168.1.103',port=5000)
