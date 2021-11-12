2b0c03fcef84df73b38e5e80f2402ca805403d7b
SAKDIPAT ONTOUM <sakdipat.2542@mail.kmutt.ac.th>
2021-11-12 13:26:59 +0700

    "test"

--- modules/dataBase/collection.py
+++ modules/dataBase/collection.py
@@ -1,21 +1,18 @@
 from pymongo import MongoClient
 
-client = MongoClient("mongodb+srv://ttp_coffee_love:DPFi0FZhUUBpCNVH@cluster0.vpotj.mongodb.net/test", tls=True,
-                     tlsAllowInvalidCertificates=True)  # host uri
-db = client.image_predition  # Select the database
+client = MongoClient("mongodb://localhost:27017") #host uri  
+db = client.image_predition #Select the database  
 image_details = db.imageData
 
-
-def addNewImage(i_name, class_name, score, time, url):
+def addNewImage(i_name,class_name,score,time,url):
     image_details.insert({
-        "file_name": i_name,
-        "class": class_name,
-        "score": score,
-        "upload_time": time,
-        "url": url
+        "file_name":i_name,
+        "class":class_name,
+        "score":score,
+        "upload_time":time,
+        "url":url
     })
-
-
+    
 def getAllImages():
     data = image_details.find()
     return data
