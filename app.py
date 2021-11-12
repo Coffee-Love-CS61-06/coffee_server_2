870c06b46ca79eb1e7b832b8b2df6158971becdc
SAKDIPAT ONTOUM <sakdipat.2542@mail.kmutt.ac.th>
2021-11-12 13:43:08 +0700

    "change name from run.py to app.py"

--- app.py
+++ app.py
@@ -5,4 +5,4 @@ model = load_model("modules/model/mobilenet_model.hdf5")
 print("model loading .... plaese wait this might take a while")
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(debug=True)
