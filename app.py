ed52febd18196c553fd5761c4f87fd8f54210f75
SAKDIPAT ONTOUM <sakdipat.2542@mail.kmutt.ac.th>
2021-11-19 11:38:58 +0700

    "test"

--- app.py
+++ app.py
@@ -5,4 +5,4 @@ model = load_model("modules/model/mobilenet_model.hdf5")
 print("model loading .... plaese wait this might take a while")
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port="8400", debug=True)
+    app.run()
