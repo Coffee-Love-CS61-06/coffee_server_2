aae8b9b75ada501a913e963ff77b1cce8b892131
Sakdipat Ontoum <sakdipat3536@gmail.com>
2021-12-06 14:08:45 +0700

    [EDIT] change port into 5000 same as first time

--- app.py
+++ app.py
@@ -5,4 +5,4 @@ model = load_model("modules/model/mobilenet_model.hdf5")
 print("model loading .... plaese wait this might take a while")
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='127.0.0.1', port="5000", debug=False)
